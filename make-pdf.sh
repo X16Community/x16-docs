db1269a354796f704d2dd5909485603f6f48da8f
stefan-b-jakobsson <stefan.b.jakobsson@gmail.com>
2023-05-11 23:31:29 +0300

    Creating PDF version with node md-to-pdf library

---
+++ make-pdf.sh
@@ -0,0 +1,17 @@
+cat header_footer.md \
+"Commander X16 Programmer's Reference Guide.md" page-break.md \
+"X16 Reference - 01 - Overview.md" page-break.md \
+"X16 Reference - 02 - Editor.md" page-break.md \
+"X16 Reference - 03 - BASIC.md" page-break.md \
+"X16 Reference - 04 - KERNAL.md" page-break.md \
+"X16 Reference - 05 - Math Library.md" page-break.md \
+"X16 Reference - 06 - Machine Language Monitor.md" page-break.md \
+"X16 Reference - 07 - Memory Map.md" page-break.md \
+"X16 Reference - 08 - Video Programming.md" page-break.md \
+"X16 Reference - 09 - Sound Programming.md" page-break.md \
+"X16 Reference - 10 - IO Programming.md" page-break.md \
+"X16 Reference - 11 - Working with CMDR-DOS.md" page-break.md \
+"X16 Reference - 12 - Hardware.md" page-break.md \
+"X16 Reference - 13 - Upgrade Guide.md" page-break.md \
+"X16 Reference - Appendix A - Sound.md" page-break.md \
+| md-to-pdf --stylesheet markdown.css > "Commander X16 Programmer's Reference Guide.pdf"
---
+++ page-break.md
@@ -0,0 +1,2 @@
+<div class="page-break"></div>  
+
